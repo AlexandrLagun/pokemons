87f9c6ef4af51f64c94b20219726b831898e3f2f
AlexandrLagun <alexanderlagun717@gmail.com>
2019-10-21 00:11:30 +0300

    Updates

---
+++ precache-manifest.84281d4f21913b189c10e93a9b9b10b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00f5ff55f2d71ec44f9f5a50caee188",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "35b9c2fb550b07c44c5c",
+    "url": "/pokemons/static/css/main.365febcd.chunk.css"
+  },
+  {
+    "revision": "5ceca1a17f71aa834e60",
+    "url": "/pokemons/static/js/2.cfe05145.chunk.js"
+  },
+  {
+    "revision": "35b9c2fb550b07c44c5c",
+    "url": "/pokemons/static/js/main.84090f28.chunk.js"
+  },
+  {
+    "revision": "120a354142afaa71d253",
+    "url": "/pokemons/static/js/runtime-main.ceb5f6bd.js"
+  }
+]);
